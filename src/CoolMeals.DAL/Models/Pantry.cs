6525e1f15d5364bafd6f10b45db702e2ddd38405
Dejmenek <dejmen14567@gmail.com>
2025-05-16 17:14:55 +0200

    chore: add Pantry and PantryIngredient models
    Added a `Pantry` class in the `CoolMeals.DAL.Models` namespace with properties for pantry identification, user association, name, and description.
    Added a `PantryIngredient` class in the same namespace with properties for ingredient identification, pantry association, quantity, unit, and expiration date.

---
+++ src/CoolMeals.DAL/Models/Pantry.cs
@@ -0,0 +1,8 @@
+﻿namespace CoolMeals.DAL.Models;
+public class Pantry
+{
+    public int PantryId { get; set; }
+    public Guid UserId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
