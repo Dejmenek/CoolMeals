026e4c4571b8ff95529ab213c1e160b36e9a4809
Dejmenek <dejmen14567@gmail.com>
2025-05-16 16:34:27 +0200

    chore: add Recipe and RecipeIngredient models
    These models represent
    recipes and their associated ingredients, respectively.
    
    `Recipe` includes properties such as `RecipeId`, `UserId`,
    `SpoonacularId`, `Title`, `ReadyInMinutes`, `Servings`,
    `ImageUrl`, and `IsFavorite`.
    
    `RecipeIngredient` includes properties such as
    `RecipeIngredientId`, `RecipeId`, `SpoonacularId`,
    `Quantity`, and `Unit`.

---
+++ src/CoolMeals.DAL/Models/Recipe.cs
@@ -0,0 +1,12 @@
+﻿namespace CoolMeals.DAL.Models;
+public class Recipe
+{
+    public int RecipeId { get; set; }
+    public Guid UserId { get; set; }
+    public int? SpoonacularId { get; set; }
+    public string? Title { get; set; }
+    public int? ReadyInMinutes { get; set; }
+    public int? Servings { get; set; }
+    public string? ImageUrl { get; set; }
+    public bool IsFavorite { get; set; }
+}
---
+++ src/CoolMeals.DAL/Models/RecipeIngredient.cs
@@ -0,0 +1,9 @@
+﻿namespace CoolMeals.DAL.Models;
+public class RecipeIngredient
+{
+    public int RecipeIngredientId { get; set; }
+    public int RecipeId { get; set; }
+    public int? SpoonacularId { get; set; }
+    public decimal? Quantity { get; set; }
+    public string? Unit { get; set; }
+}
