3b4c5d93c75254ba8726227fe080bedddf4b0ad4
Dejmenek <dejmen14567@gmail.com>
2025-05-16 16:32:47 +0200

    chore: add custom ApplicationUser class
    Introduced a new `ApplicationUser` class in the `CoolMeals.DAL.Models`
    namespace, inheriting from `IdentityUser<Guid>`. This custom user
    model includes `FirstName` and `LastName` properties (both `string`
    and initialized to empty strings) to store additional user details.

---
+++ src/CoolMeals.DAL/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CoolMeals.DAL.Models;
+public class ApplicationUser : IdentityUser<Guid>
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
