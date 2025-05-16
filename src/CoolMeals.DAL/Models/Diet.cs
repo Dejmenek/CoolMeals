8afe01fe9aa243156716fd58001fccc9c8bb63ac
Dejmenek <dejmen14567@gmail.com>
2025-05-16 16:35:59 +0200

    chore: add Diet and UserDiet models
    Introduced two new classes in the CoolMeals.DAL.Models namespace:
    - `Diet`: Represents a diet entity with properties for ID, name,
      and description.
    - `UserDiet`: Establishes a relationship between users and diets
      by storing their respective IDs.
    
    These changes lay the groundwork for managing diets and associating
    them with users in the application.

---
+++ src/CoolMeals.DAL/Models/Diet.cs
@@ -0,0 +1,7 @@
+﻿namespace CoolMeals.DAL.Models;
+public class Diet
+{
+    public int DietId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
