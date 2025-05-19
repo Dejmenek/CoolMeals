7fb133f69d7e8a9a080186212d8f2405206daefe
Dejmenek <dejmen14567@gmail.com>
2025-05-19 22:02:02 +0200

    chore: add ApplicationDbContext for database and Identity integration
    Introduce ApplicationDbContext in CoolMeals.DAL.Database, extending
    IdentityDbContext with custom user and role types using Guid as the
    primary key. Add DbSet properties for various entities, including
    Diet, Recipe, MealPlan, and more. Override OnModelCreating to apply
    entity configurations.

---
+++ src/CoolMeals.DAL/Database/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using CoolMeals.DAL.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoolMeals.DAL.Database;
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+{
+    public DbSet<Diet> Diets { get; set; }
+    public DbSet<Instruction> Instructions { get; set; }
+    public DbSet<Intolerance> Intolerances { get; set; }
+    public DbSet<MealPlan> MealPlans { get; set; }
+    public DbSet<MealPlanEntry> MealPlanEntries { get; set; }
+    public DbSet<Pantry> Pantries { get; set; }
+    public DbSet<PantryIngredient> PantryIngredients { get; set; }
+    public DbSet<Recipe> Recipes { get; set; }
+    public DbSet<RecipeIngredient> RecipeIngredients { get; set; }
+    public DbSet<ShoppingList> ShoppingLists { get; set; }
+    public DbSet<ShoppingListItem> ShoppingListItems { get; set; }
+    public DbSet<UserDiet> UserDiets { get; set; }
+    public DbSet<UserIntolerance> UserIntolerances { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
