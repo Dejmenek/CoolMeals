32075c4b98b081e88940deadf0a74a2596acf0a6
Dejmenek <dejmen14567@gmail.com>
2025-05-20 16:28:00 +0200

    chore: add navigation properties to ShoppingList
    Two new properties were added to the `ShoppingList` class:
    1. `User`: Represents the user associated with the shopping list.
    2. `ShoppingListItems`: Represents the collection of items in
       the shopping list.

--- src/CoolMeals.DAL/Models/ShoppingList.cs
+++ src/CoolMeals.DAL/Models/ShoppingList.cs
@@ -4,4 +4,6 @@ public class ShoppingList
     public int ShoppingListId { get; set; }
     public int UserId { get; set; }
     public string Title { get; set; } = string.Empty;
+    public ApplicationUser User { get; set; } = null!;
+    public ICollection<ShoppingListItem> ShoppingListItems { get; } = new List<ShoppingListItem>();
 }
