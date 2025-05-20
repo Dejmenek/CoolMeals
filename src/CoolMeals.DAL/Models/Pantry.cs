132e841899f107157dc10a7425b842869b064642
Dejmenek <dejmen14567@gmail.com>
2025-05-20 16:16:55 +0200

    chore: add navigation properties to Pantry class
    Two new properties were added to the `Pantry` class in `Pantry.cs`:
    1. `User`: Links the pantry to an `ApplicationUser`, representing the
       owner of the pantry. It is initialized with a non-nullable default
       value (`null!`).
    2. `PantryIngredients`: Introduces a collection of `PantryIngredient`
       objects, initialized with a new `List<PantryIngredient>()`.
    
    These changes establish relationships between the `Pantry` class,
    `ApplicationUser`, and `PantryIngredient`, enabling better management
    of pantry ownership and its associated ingredients.

--- src/CoolMeals.DAL/Models/Pantry.cs
+++ src/CoolMeals.DAL/Models/Pantry.cs
@@ -5,4 +5,6 @@ public class Pantry
     public Guid UserId { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public ApplicationUser User { get; set; } = null!;
+    public ICollection<PantryIngredient> PantryIngredients { get; } = new List<PantryIngredient>();
 }
