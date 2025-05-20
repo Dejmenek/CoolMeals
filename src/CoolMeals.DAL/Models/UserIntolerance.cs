16c3139c1dbd67307798151577fcebc03ca4d7b7
Dejmenek <dejmen14567@gmail.com>
2025-05-20 16:31:33 +0200

    chore: add navigation properties to UserIntolerance class
    Two new properties were added to the `UserIntolerance` class:
    1. `Intolerance` of type `Intolerance`.
    2. `User` of type `ApplicationUser`.
    
    These properties establish navigation relationships between
    `UserIntolerance` and the `Intolerance` and `ApplicationUser`
    entities.

--- src/CoolMeals.DAL/Models/UserIntolerance.cs
+++ src/CoolMeals.DAL/Models/UserIntolerance.cs
@@ -3,4 +3,6 @@ public class UserIntolerance
 {
     public int IntoleranceId { get; set; }
     public int UserId { get; set; }
+    public Intolerance Intolerance { get; set; } = null!;
+    public ApplicationUser User { get; set; } = null!;
 }
