39ee37919bb4dd1ef96b2cbc23d321daa93a356a
Dejmenek <dejmen14567@gmail.com>
2025-05-20 16:03:37 +0200

    chore: add collection properties to ApplicationUser for relationships
    Enhanced the ApplicationUser class by adding new collection
    properties: UserDiets, UserIntolerances, Pantries, MealPlans,
    ShoppingLists, and Recipes. These properties represent one-to-many
    relationships between a user and various domain entities, such as
    diets, intolerances, pantries, meal plans, shopping lists, and
    recipes.

--- src/CoolMeals.DAL/Models/ApplicationUser.cs
+++ src/CoolMeals.DAL/Models/ApplicationUser.cs
@@ -5,4 +5,10 @@ public class ApplicationUser : IdentityUser<Guid>
 {
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
+    public ICollection<UserDiet> UserDiets { get; } = new List<UserDiet>();
+    public ICollection<UserIntolerance> UserIntolerances { get; } = new List<UserIntolerance>();
+    public ICollection<Pantry> Pantries { get; } = new List<Pantry>();
+    public ICollection<MealPlan> MealPlans { get; } = new List<MealPlan>();
+    public ICollection<ShoppingList> ShoppingLists { get; } = new List<ShoppingList>();
+    public ICollection<Recipe> Recipes { get; } = new List<Recipe>();
 }
