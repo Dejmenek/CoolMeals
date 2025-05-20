54aeb6671cdef3ed9e5b73668eee0f83c2061c87
Dejmenek <dejmen14567@gmail.com>
2025-05-20 16:26:02 +0200

    chore: update RecipeIngredient properties for consistency
    The `SpoonacularId` property in the `RecipeIngredient` class
    was changed from nullable (`int?`) to non-nullable (`int`)
    to ensure consistent data handling.
    
    A new `Recipe` navigation property of type `Recipe` was added to the
    `RecipeIngredient` class.

--- src/CoolMeals.DAL/Models/RecipeIngredient.cs
+++ src/CoolMeals.DAL/Models/RecipeIngredient.cs
@@ -3,7 +3,8 @@ public class RecipeIngredient
 {
     public int RecipeIngredientId { get; set; }
     public int RecipeId { get; set; }
-    public int? SpoonacularId { get; set; }
+    public int SpoonacularId { get; set; }
     public decimal? Quantity { get; set; }
     public string? Unit { get; set; }
+    public Recipe Recipe { get; set; } = null!;
 }
