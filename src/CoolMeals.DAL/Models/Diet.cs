bc33d58c5f067c42795f16b006cda2248ce07fcb
Dejmenek <dejmen14567@gmail.com>
2025-05-20 16:04:30 +0200

    chore: add UserDiets property to Diet class for relationship
    Introduce a `UserDiets` property in the `Diet` class to represent
    a one-to-many relationship with `UserDiet` objects.

--- src/CoolMeals.DAL/Models/Diet.cs
+++ src/CoolMeals.DAL/Models/Diet.cs
@@ -4,4 +4,5 @@ public class Diet
     public int DietId { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public ICollection<UserDiet> UserDiets { get; } = new List<UserDiet>();
 }
