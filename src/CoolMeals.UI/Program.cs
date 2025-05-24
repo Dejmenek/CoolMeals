07c6b05581a2658936518d8f47def67c76138e36
Dejmenek <dejmen14567@gmail.com>
2025-05-24 23:06:04 +0200

    chore: add data access layer and connection string
    Added a reference to the `CoolMeals.DAL` project in the
    `CoolMeals.UI.csproj` file to integrate the data access layer.
    Updated `Program.cs` to include necessary namespaces, register
    data access services, and implement a check for database
    initialization in development mode. Also, added a new
    `ConnectionStrings` section in `appsettings.json` to define
    the connection string for the local SQL Server database.

--- src/CoolMeals.UI/Program.cs
+++ src/CoolMeals.UI/Program.cs
@@ -1,9 +1,23 @@
+using CoolMeals.DAL;
+using CoolMeals.DAL.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDataAccessLayer(builder.Configuration);
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
