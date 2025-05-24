742160ede52d5fe650bd7c18279b5eb172c706cd
Dejmenek <dejmen14567@gmail.com>
2025-05-24 23:05:37 +0200

    chore: add DependencyInjection for data access layer
    Introduces a new `DependencyInjection` class in the `CoolMeals.DAL` namespace. This class provides methods to configure the data access layer and database context. The `AddDataAccessLayer` method registers the necessary services, while the `AddDatabase` method sets up the `ApplicationDbContext` with a SQL Server connection string from the configuration settings.

---
+++ src/CoolMeals.DAL/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using CoolMeals.DAL.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoolMeals.DAL;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services
+            .AddDatabase(configuration);
+    }
+
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        string? connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+
+        return services;
+    }
+}
